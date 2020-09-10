9e14ff586e77013dfac277d337f05d6e62a771f0
@sulli8 <sullivan.sextius@gmail.com>
2020-09-10 09:34:53 +0200

    modification des chemins des fichier et des dossier du projet et mise en place de balise pour alléger le code, modification extension des fichier

---
+++ medico/include/header.php
@@ -0,0 +1,58 @@
+<header class="main_menu home_menu">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.php"> <img src="../img/logo.png" alt="logo"> </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse main-menu-item justify-content-center"
+                        id="navbarSupportedContent">
+                        <ul class="navbar-nav align-items-center">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">about</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="Doctor.php">Doctors</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pages
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="services.php">services</a>
+                                    <a class="dropdown-item" href="dep.php">depertments</a>
+                                    <a class="dropdown-item" href="elements.php">Elements</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="blog.php" id="navbarDropdown_1"
+                                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    blog
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                    <a class="dropdown-item" href="blog.php">blog</a>
+                                    <a class="dropdown-item" href="single-blog.php">Single blog</a>
+                                </div>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a class="btn_2 d-none d-lg-block" href="#">HOT LINE- 09856</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
