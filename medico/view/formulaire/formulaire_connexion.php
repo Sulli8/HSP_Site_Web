ed0dcb026ce00dfcb36dbf15c2d788f82d8ba058
VASONE_An <VASONE_An@lprs.local>
2020-09-21 14:46:03 +0200

    modification css connexion

--- medico/view/formulaire/formulaire_connexion.php
+++ medico/view/formulaire/formulaire_connexion.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 
-    <meta charset="utf-8">
-      <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/HSP_Site_Web/medico/manager/manager.php");
-      $manager = new Manager();
-$manager->include_forms();
-       ?>
-    <title>HSP | Connexion</title>
+<meta charset="utf-8">
+<?php
+  require_once($_SERVER['DOCUMENT_ROOT']."/HSP_Site_Web/medico/manager/manager.php");
+  $manager = new Manager();
+  $manager->include_forms();
+?>
+<title>HSP | Connexion</title>
+<link rel="stylesheet" href="formulaire_connexion_style.css">
+
+<body>
+
+  <?php $manager->include_header();?>
+
+  <form action="../../traitement/traitement_formulaire_connexion.php" method="post">
+
+    <div class="form-group">
+      <label for="exampleInputEmail1" >Entrez votre adresse e-mail</label>
+      <input type="email" name="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse e-mail">
+      <label for="exampleInputPassword1">Entrez votre mot de passe</label>
+      <input type="password"  name="mdp" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
 
 
-  <body>
-      <?php $manager->include_header();?>
+      <a href="../../traitement/traitement_mot_de_passe-oublie.php">Mot de passe oublié ?</a>
 
-    <form <?php $manager->css_forms(); ?>action="../../traitement/traitement_formulaire_connexion.php" method="post">
-  <div class="form-group">
-    <label for="exampleInputEmail1" >Email address</label>
-    <input type="email" name="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password"  name="mdp" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
+      <a href="formulaire_inscription.php">Vous n'avez pas de compte ?</a>
 
-  <div>        <a  <?php $manager->css_a(); ?>href="../../traitement/traitement_mot_de_passe-oublie.php">Mot de passe oublié ?</a>
-          <a  <?php $manager->css_a(); ?>href="formulaire_inscription.php">Vous n'avez pas de compte ?</a></div>
-  <button type="submit" class="btn btn-primary">Connexion</button>
-</form>
+      <button type="submit" class="btn btn-primary">Connexion</button>
 
-  </body>
+      <a class="return-home" href="../index.php">Retour à l'accueil</a>
+
+    </div>
+
+  </form>
+
+</body>
 </html>
